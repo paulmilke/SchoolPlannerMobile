da3c11e80c0a064d165b514708fddb5798334d7b
Paul Milke <milkepaul1@gmail.com>
2023-12-08 14:04:41 -0800

    Finishes features for part C4.
    This includes a new service for notifications.
    Also updates to my edit course page to allow for notifications.
    Also paves the way for part C5.

---
+++ MobileApp_C971_LAP2_PaulMilke/Services/INotificationService.cs
@@ -0,0 +1,12 @@
+﻿using Plugin.LocalNotification;
+
+
+namespace MobileApp_C971_LAP2_PaulMilke.Services
+{
+    public interface INotificationService
+    {
+        Task<bool> AreNotificationsEnabledAsync();
+        Task RequestNotificationPermissionAsync(); 
+        Task ScheduleNotificationAsync(NotificationRequest notifcaionRequest);
+    }
+}
---
+++ MobileApp_C971_LAP2_PaulMilke/Services/NotificationService.cs
@@ -0,0 +1,27 @@
+﻿using Plugin.LocalNotification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileApp_C971_LAP2_PaulMilke.Services
+{
+    class NotificationService : INotificationService
+    {
+        public async Task<bool> AreNotificationsEnabledAsync()
+        {
+            return await LocalNotificationCenter.Current.AreNotificationsEnabled();
+        }
+
+        public async Task RequestNotificationPermissionAsync()
+        {
+            await LocalNotificationCenter.Current.RequestNotificationPermission(); 
+        }
+
+        public async Task ScheduleNotificationAsync(Plugin.LocalNotification.NotificationRequest notifcaionRequest)
+        {
+            await LocalNotificationCenter.Current.Show(notifcaionRequest); 
+        }
+    }
+}
