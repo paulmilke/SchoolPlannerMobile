47c3e7f0de17292b99aca14332a1261ab6abf560
paulmilke <milkepaul1@gmail.com>
2024-09-27 16:44:14 -0700

    calls new save term POST from view model

--- MobileApp_C971_LAP2_PaulMilke/View Model/AddNewTermPopupViewModel.cs
+++ MobileApp_C971_LAP2_PaulMilke/View Model/AddNewTermPopupViewModel.cs
@@ -111,7 +111,9 @@ namespace MobileApp_C971_LAP2_PaulMilke.View_Model
                 else
                 {
                     Term newTerm = new Term(titleText, startDate, endDate);
-                    await _schoolDatabase.SaveTermAsync(newTerm);
+                    //await _schoolDatabase.SaveTermAsync(newTerm);
+                    RestService restApi = new RestService();
+                    await restApi.SaveNewTermAsync(newTerm);
                 }
 
             }
